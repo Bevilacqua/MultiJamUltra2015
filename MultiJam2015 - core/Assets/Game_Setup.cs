174a830a341d9876a537d2ed8a15c2bb5e56e930
Bevilacqua <jacobbev@gmail.com>
2015-04-25 14:19:53 -0400

    Game loop structured

--- MultiJam2015 - core/Assets/Game_Setup.cs
+++ MultiJam2015 - core/Assets/Game_Setup.cs
@@ -7,31 +7,47 @@ public class Game_Setup : MonoBehaviour {
     public int cellsPerRow = 5;
 
     private int totalCells;
+    private GameObject[,] nodes;
 
-    public GameObject pre_ExampleCell;
+    public GameObject pre_Node, pre_CenterBlock, pre_FinishLine;
 
     private float cellWidth, rowHeight;
     private Camera camera;
+    private Player playerOne;
+    private Player playerTwo;
 
 	void Start () {
         camera = Camera.main;
+
+        playerOne = GameObject.Find("PlayerOne").GetComponent<Player>();
+        playerTwo = GameObject.Find("PlayerTwo").GetComponent<Player>();
 
         totalCells = ((cellsPerRow * 2) + 3);
         //In pixels (screen)
         cellWidth = camera.pixelWidth / totalCells; //Additional three account for left and right finish line and center deadspace
         rowHeight = (camera.pixelHeight - 100) / (numberOfRows);
 
+        nodes = new GameObject[numberOfRows, totalCells];
 
         Vector2 cellScale = new Vector2(5, 5);
 
-        pre_ExampleCell.transform.localScale = cellScale;
+        pre_Node.transform.localScale = cellScale;
 
         for(int x = 0; x < numberOfRows; x++) {
-            for (int y = 0; y < totalCells; y++)
+            for (int y =  0; y < totalCells; y++)
             {
-                GameObject node = (GameObject) Instantiate(pre_ExampleCell, camera.ScreenToWorldPoint(new Vector3(cellWidth * y + cellWidth - (cellWidth / 2), (rowHeight * x) + rowHeight - (rowHeight / 2), 1)), pre_ExampleCell.transform.localRotation);
-                if(y == 6)
-                    node.GetComponent<SpriteRenderer>().color = Color.gray;
+                if (y == 6)
+                {
+                    nodes[x, y] = (GameObject)Instantiate(pre_CenterBlock, camera.ScreenToWorldPoint(new Vector3(cellWidth * y + cellWidth - (cellWidth / 2), (rowHeight * x) + rowHeight - (rowHeight / 2), 1)), pre_CenterBlock.transform.localRotation);
+                    continue;
+                }
+                if (y == 0 || y == 12)
+                {
+                    nodes[x, y] = (GameObject)Instantiate(pre_FinishLine, camera.ScreenToWorldPoint(new Vector3(cellWidth * y + cellWidth - (cellWidth / 2), (rowHeight * x) + rowHeight - (rowHeight / 2), 1)), pre_CenterBlock.transform.localRotation);
+                    continue;
+                }
+                GameObject node = (GameObject) Instantiate(pre_Node, camera.ScreenToWorldPoint(new Vector3(cellWidth * y + cellWidth - (cellWidth / 2), (rowHeight * x) + rowHeight - (rowHeight / 2), 1)), pre_Node.transform.localRotation);
+               
                 if (y == 1 || y == 11)
                 {
                     if (x == 0) node.GetComponent<SpriteRenderer>().color = new Color(Color.red.r, Color.red.g, Color.red.b, .25f);
@@ -40,16 +56,42 @@ public class Game_Setup : MonoBehaviour {
                     if (x == 3) node.GetComponent<SpriteRenderer>().color = new Color(Color.yellow.r, Color.yellow.g, Color.yellow.b, .25f);
                     if (x == 4) node.GetComponent<SpriteRenderer>().color = new Color(Color.cyan.r, Color.cyan.g, Color.cyan.b, .25f);
                 }
-                if(y == 0 || y == 12)
-                {
-                    node.GetComponent<SpriteRenderer>().color = new Color(Color.white.r, Color.white.g, Color.white.b, .25f);
-                }
+
+                nodes[x, y] = node;
             }
         }
 
+        playerOne.setTurn(true);
+        playerTwo.setTurn(false);
 	}
 	
 	void Update () {
-	
+        if (playerOne.getCurrentTurn())
+        {
+            if (playerOne.getChosen())
+            {
+                //Algorithm step 2
+                //2.5/ 2.5a
+                //3
+                //4
+                //5
+                playerOne.resetPlayer(); //6
+                playerOne.setTurn(false); //6
+                playerTwo.setTurn(true); //6
+            }
+        } else if(playerTwo.getCurrentTurn())
+        {
+            if (playerOne.getChosen())
+            {
+                //Algorithm step 2
+                //2.5/ 2.5a
+                //3
+                //4
+                //5
+                playerTwo.resetPlayer(); //6
+                playerTwo.setTurn(false); //6
+                playerOne.setTurn(true); //6
+            }
+        }
 	}
 }
---
+++ MultiJam2015 - core/Assets/Player.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private int position; //0 == bottom
+    private Color color;
+
+    //Color Definitions
+    private Color red = Color.red;
+    private Color green = Color.green;
+    private Color blue = Color.blue;
+    private Color yellow = Color.yellow;
+    private Color cyan = Color.cyan;
+    private Color inactive = new Color(Color.white.r, Color.white.g, Color.white.b, .25f);
+
+    private Color[] colors;
+
+    private bool currentTurn = false;
+    private bool locationChosen = false;
+
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode shoot;
+
+    void Start () {
+	    colors = new Color[] {red, green, blue, yellow, cyan};
+	}
+
+	void Update () {
+        if (currentTurn)
+        {
+            if (Input.GetKeyDown(up) && position < 4)
+            {
+                position++;
+            } else if(Input.GetKeyDown(down) && position > 0)
+            {
+                position--;
+            }
+            
+            if(Input.GetKeyDown(shoot))
+            {
+                locationChosen = true;
+            }
+        }
+	}
+
+    public bool getCurrentTurn()
+    {
+        return this.currentTurn;
+    }
+
+    public void setTurn(bool turn)
+    {
+        this.currentTurn = turn;
+    }
+
+    public bool getChosen()
+    {
+        return this.locationChosen;
+    }
+
+    public void resetPlayer()
+    {
+        this.position = 0;
+        this.locationChosen = false;
+    }
+
+    public int getPosition()
+    {
+        return this.position;
+    }
+}
